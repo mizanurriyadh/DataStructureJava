97f7db730c67e2fca6d5a12313c80e3c171e3837
mizanurriyadh <mizanurriyadh@ymail.com>
2017-06-02 02:27:18 +0600

    Remove from Queue

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -7,7 +7,13 @@ public class Main {
         System.out.println("Add ("+a+")");
     }
     
-    
+    static void remove(Queue q)
+    {
+        System.out.println("Remove -> ");
+        Integer a = (Integer) q.remove();
+        System.out.println(a);
+        System.out.println("Queue: "+q);
+    }
     
     public static void main(String[] args) {
         Queue<Integer> q = new PriorityQueue<Integer>();
@@ -16,7 +22,10 @@ public class Main {
             add(q, i);
         }
         
-        
+        for(int i=0; i<5; i++)
+        {
+            remove(q);
+        }
     }
     
 }
